e233381faa81c7d26952cc1bd6bdddb6fc1d5d6d
farhan-alam18 <farhanalam1113@gmail.com>
2024-08-28 16:27:03 +0530

    Random Color Generator Done

--- random-color-generator/main.js
+++ random-color-generator/main.js
@@ -1,8 +1,9 @@
-// Hex Color Generator : 
+// Hex Color Generator :
 
 const hexButton = document.querySelector(".hex-button");
-const hexColorValue = document.querySelector(".hex-value")
-const hexContainer = document.querySelector(".hex-container")
+const hexColorValue = document.querySelector(".hex-value");
+const hexContainer = document.querySelector(".hex-container");
+const hexCopy = document.querySelector(".hex-copy");
 
 const createRandHexColor = () => {
   let charSet = "0123456789ABCDEF";
@@ -12,20 +13,46 @@ const createRandHexColor = () => {
     hexOutput += charSet.charAt(Math.floor(Math.random() * charSet.length));
   }
 
-  hexColorValue.textContent = `#${hexOutput}`
-  hexContainer.style.backgroundColor = `#${hexOutput}`
-  hexButton.style.color = `#${hexOutput}`
+  hexColorValue.textContent = `#${hexOutput}`;
+  hexContainer.style.backgroundColor = `#${hexOutput}`;
+  hexButton.style.color = `#${hexOutput}`;
 };
 
 hexButton.addEventListener("click", createRandHexColor);
 
+// RGB Color Generator :
 
-// RGB Color Generator : 
-
-const rgbButton = document.querySelector('.rgb-button')
+const rgbButton = document.querySelector(".rgb-button");
+const redInputRange = document.querySelector("#red");
+const greenInputRange = document.querySelector("#green");
+const blueInputRange = document.querySelector("#blue");
+const rgbContainer = document.querySelector(".rgb-container");
+const rgbCopy = document.querySelector(".rgb-copy");
+const rgbValue = document.querySelector(".rgb-value");
 
 const createRandRgbColor = () => {
-  
-}
+  let redValue = redInputRange.value;
+  let greenValue = greenInputRange.value;
+  let blueValue = blueInputRange.value;
 
-rgbButton.addEventListener('click', createRandRgbColor)
+  rgbContainer.style.backgroundColor = `rgb(${redValue}, ${greenValue},${blueValue})`;
+
+  rgbValue.textContent = `rgb(${redValue}, ${greenValue},${blueValue})`;
+};
+
+rgbButton.addEventListener("click", createRandRgbColor);
+
+// Copy to clipboard :
+const copyHexToClipboard = () => {
+  navigator.clipboard.writeText(hexColorValue.textContent);
+  alert(`${hexColorValue.textContent} Hex is copied`);
+};
+
+hexCopy.addEventListener("click", copyHexToClipboard);
+
+const copyRgbToClipboard = () => {
+  navigator.clipboard.writeText(rgbContainer.style.backgroundColor);
+  alert(`${rgbContainer.style.backgroundColor} RGB is copied`);
+};
+
+rgbCopy.addEventListener("click", copyRgbToClipboard);
